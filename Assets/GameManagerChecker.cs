83b5458af812c270a91d52f23fb4af8d624f653f
Trenton Pottruff <jayitinc@gmail.com>
2020-03-22 20:38:51 -0400

    Game Manager

---
+++ Assets/GameManagerChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerChecker : MonoBehaviour {
+	private void Awake() {
+		int sceneCount = SceneManager.sceneCount;
+		List<Scene> scenes = new List<Scene>();
+		bool persistentSceneLoaded = false;
+
+		for (int i = 0; i < sceneCount; i++) {
+			Scene scene = SceneManager.GetSceneAt(i);
+			if (scene.buildIndex == 0) {
+				Debug.Log("Persistent scene is loaded");
+				persistentSceneLoaded = true;
+				continue;
+			}
+			scenes.Add(scene);
+		}
+
+		if (!persistentSceneLoaded) {
+			Debug.LogWarning("Persistent scene is not loaded, reloading...");
+			SceneManager.LoadScene((int)SceneIndexes.MANAGER, LoadSceneMode.Additive);
+		}
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+
+	private void Awake() {
+		instance = this;
+
+		if (SceneManager.sceneCount < 2) {
+			SceneManager.LoadSceneAsync((int)SceneIndexes.MAIN_MENU, LoadSceneMode.Additive);
+		}
+	}
+
+	public void LoadGame() {
+		SceneManager.UnloadSceneAsync((int)SceneIndexes.MAIN_MENU);
+		SceneManager.LoadSceneAsync((int)SceneIndexes.GAME, LoadSceneMode.Additive);
+	}
+}
+
+public enum SceneIndexes {
+	MANAGER = 0,
+	MAIN_MENU = 1,
+	GAME = 2
+}

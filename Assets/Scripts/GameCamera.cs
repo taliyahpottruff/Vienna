77d3ca203d2e84b668abed5e735646ed2cc8d5bf
Trenton Pottruff <jayitinc@gmail.com>
2020-03-24 21:33:09 -0400

    Smooth Camera + Placeholder Art

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class GameCamera : MonoBehaviour {
+    public Transform target;
+
+    public float smoothSpeed = 5f;
+    private Vector3 offset = new Vector3(0, -0.5f, -10);
+
+    public float size = 5f;
+
+    private new Camera camera;
+
+    private void Start() {
+        camera = GetComponent<Camera>();
+    }
+
+    public void FixedUpdate() {
+        try {
+            if (target == null) return;
+
+            Vector3 newPosition = target.position + offset;
+            newPosition.z = -10f;
+
+            //Smooth
+            transform.position -= (transform.position - newPosition) * smoothSpeed * Time.deltaTime;
+            camera.orthographicSize -= (camera.orthographicSize - size) * smoothSpeed * Time.deltaTime;
+        } catch (Exception e) {
+            Debug.LogError(e.ToString());
+        }
+    }
+
+    public void SetTarget(Transform target) {
+        this.target = target;
+    }
+}

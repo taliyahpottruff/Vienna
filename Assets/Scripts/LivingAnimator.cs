57108a9101d58d687a591642b2941c53e31c512e
Trenton Pottruff <tpottruff@gmail.com>
2020-12-03 23:50:49 -0500

    Full body clothing

--- Assets/Scripts/LivingAnimator.cs
+++ Assets/Scripts/LivingAnimator.cs
@@ -2,75 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class LivingAnimator : MonoBehaviour {
-	public float animationSpeed = 0.1f;
-	public List<Sprite> upSprites, downSprites, sideSprites;
-	public string hairType = "ShortHair";
-	public SpriteRenderer hairRenderer;
-	
-	private SpriteRenderer sr;
-	private Rigidbody2D rb;
+namespace Vienna {
+	[RequireComponent(typeof(SpriteRenderer))]
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class LivingAnimator : MonoBehaviour {
+		public float animationSpeed = 0.1f;
+		public List<Sprite> upSprites, downSprites, sideSprites;
+		public string hairType = "ShortHair", topType = "T-Shirt", bottomType = "Pants";
+		public SpriteRenderer hairRenderer, topRenderer, bottomRenderer;
 
-	private Direction direction = Direction.Down;
-	private bool moving;
-	private Sprite[] hairSprites;
+		private SpriteRenderer sr;
+		private Rigidbody2D rb;
 
-	private void Awake() {
-		sr = GetComponent<SpriteRenderer>();
-		rb = GetComponent<Rigidbody2D>();
-	}
+		private Direction direction = Direction.Down;
+		private bool moving;
+		private Sprite[] hairSprites, topSprites, bottomSprites;
 
-	private void Start() {
-		LoadHairSprites();
-		StartCoroutine(Animate());
-	}
+		private void Awake() {
+			sr = GetComponent<SpriteRenderer>();
+			rb = GetComponent<Rigidbody2D>();
+		}
 
-	private void Update() {
-		Vector2 movementVector = rb.velocity.normalized;
-		moving = !movementVector.Equals(Vector2.zero);
-		if (moving) { //First check if movement is even happening
-			direction = DirectionUtility.VectorToDirection(movementVector);
+		private void Start() {
+			LoadSprites();
+			StartCoroutine(Animate());
+		}
 
-			if (direction == Direction.Left) {
-				transform.localScale = new Vector3(-1, 1, 1);
-			} else {
-				transform.localScale = new Vector3(1, 1, 1);
+		private void Update() {
+			Vector2 movementVector = rb.velocity.normalized;
+			moving = !movementVector.Equals(Vector2.zero);
+			if (moving) { //First check if movement is even happening
+				direction = DirectionUtility.VectorToDirection(movementVector);
+
+				if (direction == Direction.Left) {
+					transform.localScale = new Vector3(-1, 1, 1);
+				} else {
+					transform.localScale = new Vector3(1, 1, 1);
+				}
 			}
 		}
-	}
 
-	private List<Sprite> GetAnimation(Direction direction) {
-		switch (direction) {
-			case Direction.Down:
-				return downSprites;
-			case Direction.Up:
-				return upSprites;
-			default:
-				return sideSprites;
+		private List<Sprite> GetAnimation(Direction direction) {
+			switch (direction) {
+				case Direction.Down:
+					return downSprites;
+				case Direction.Up:
+					return upSprites;
+				default:
+					return sideSprites;
+			}
 		}
-	}
 
-	private void LoadHairSprites() {
-		hairSprites = Resources.LoadAll<Sprite>($"Sprites/Hair/{hairType}");
-    }
+        #region Load Sprites
+		private void LoadSprites() {
+			LoadHairSprites();
+			LoadTopSprites();
+			LoadBottomSprites();
+        }
 
-	private IEnumerator Animate() {
-		int index = 0;
-		while (true) {
-			List<Sprite> animation = GetAnimation(direction);
-			if (index >= animation.Count || !moving) index = 0;
-			
-			//Set main sprite
-			sr.sprite = animation[index];
-			var animIndex = int.Parse(animation[index].name.Split('_')[1]);
+        private void LoadHairSprites() {
+			hairSprites = Resources.LoadAll<Sprite>($"Sprites/Hair/{hairType}");
+		}
 
-			//Set additional sprites
-			if (hairRenderer != null) hairRenderer.sprite = hairSprites[animIndex];
+		private void LoadTopSprites() {
+			topSprites = Resources.LoadAll<Sprite>($"Sprites/Tops/{topType}");
+        }
 
-			index++;
-			yield return new WaitForSeconds(animationSpeed);
+		private void LoadBottomSprites() {
+			bottomSprites = Resources.LoadAll<Sprite>($"Sprites/Bottoms/{bottomType}");
+        }
+        #endregion
+
+        private IEnumerator Animate() {
+			int index = 0;
+			while (true) {
+				List<Sprite> animation = GetAnimation(direction);
+				if (index >= animation.Count || !moving) index = 0;
+
+				//Set main sprite
+				sr.sprite = animation[index];
+				var animIndex = int.Parse(animation[index].name.Split('_')[1]);
+
+				//Set additional sprites
+				if (hairRenderer != null) hairRenderer.sprite = Utils.GetSpriteFromArray(animIndex, hairSprites, true);
+				if (topRenderer != null) topRenderer.sprite = Utils.GetSpriteFromArray(animIndex, topSprites, true);
+				if (bottomRenderer != null) bottomRenderer.sprite = Utils.GetSpriteFromArray(animIndex, bottomSprites, true);
+
+				index++;
+				yield return new WaitForSeconds(animationSpeed);
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Vienna {
+    public static class Utils {
+        public static Sprite GetSpriteFromArray(int index, Sprite[] sprites, bool getLast = false) {
+            if (sprites.Length < 1) return null;
+            if (index >= sprites.Length) return (getLast) ? sprites[sprites.Length - 1] : sprites[0];
+
+            return sprites[index];
+        }
+    }
+}

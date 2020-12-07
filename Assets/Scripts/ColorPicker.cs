7cedf448a358531047453bc21e8248680bdd1652
Trenton Pottruff <tpottruff@gmail.com>
2020-12-07 08:07:33 -0500

    Trying to get a color picker

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Vienna {
+    [RequireComponent(typeof(RawImage))]
+    public class ColorPicker : MonoBehaviour {
+        RawImage image;
+
+        private void Start() {
+            image = GetComponent<RawImage>();
+
+            GenerateGradient();
+        }
+
+        private void GenerateGradient() {
+            Texture2D texture = new Texture2D(256, 256);
+
+            
+               for (int x = 0; x < texture.width; x++) {
+                for (int y = 0; y < texture.height; y++) {
+                    var h = (float)x / (float)texture.width;
+                    float s = (float)y / (float)texture.height;
+                    texture.SetPixel(x, y, Color.HSVToRGB(h, s, 1));
+                }
+            }
+            texture.Apply();
+
+            image.texture = texture;
+        }
+    }
+}
---
+++ Assets/Scripts/ColorSelectorHandle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Vienna {
+    public class ColorSelectorHandle : Selectable {
+        public override void OnPointerDown(PointerEventData eventData) {
+            base.OnPointerDown(eventData);
+            Debug.Log("Move");
+        }
+
+        public override void OnPointerUp(PointerEventData eventData) {
+            base.OnPointerUp(eventData);
+            Debug.Log("Stop");
+        }
+    }
+}

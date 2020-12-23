a6c22d3387e0cc01d5deaeba38cc0ac08bfbad40
Trenton Pottruff <tpottruff@gmail.com>
2020-12-22 20:40:40 -0500

    Battle Manager

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Grid {
+    public class BattleManager : MonoBehaviour {
+        public GameObject gridSelector;
+        public LayerMask gridSelectionMask;
+        public TextMeshProUGUI turnDisplay;
+
+        private Controls controls;
+        private Vector2 mousePosition;
+        private Vector3 hoveredPosition;
+        private bool gridHovered = false;
+        private RaycastHit hit;
+        private List<Faction> factions = new List<Faction>();
+        [SerializeField]
+        private int phase = 0, turn = 1;
+        [SerializeField]
+        private GridPlayerController[] player;
+
+        private void Awake() {
+            factions.Add(new Faction(player, true));
+        }
+
+        private void Start() {
+            controls = new Controls();
+            controls.UI.Select.performed += Select_performed;
+            controls.UI.PointerPosition.performed += ctx => mousePosition = ctx.ReadValue<Vector2>();
+            controls.Enable();
+        }
+
+        private void Update() {
+            if (factions[phase].isPlayer) {
+                // Handle mouse
+                var ray = Camera.main.ScreenPointToRay(mousePosition);
+                gridHovered = Physics.Raycast(ray, out hit, Mathf.Infinity, gridSelectionMask);
+                var onGrid = gridHovered && hit.transform.tag.Equals("Grid");
+                gridSelector.SetActive(onGrid);
+
+                if (onGrid) {
+                    var position = hit.point;
+                    position.x = Mathf.Round(position.x);
+                    position.z = Mathf.Round(position.z);
+                    hoveredPosition = position;
+                    gridSelector.transform.position = new Vector3(position.x, position.y + 0.01f, position.z);
+                }
+            } else {
+                if (gridSelector.activeSelf) gridSelector.SetActive(false);
+            }
+        }
+
+        public void NextPhase() {
+            if (++phase >= factions.Count) {
+                phase = 0;
+                turn++;
+            }
+
+            if (!factions[phase].isPlayer) {
+                NextPhase();
+            }
+
+            turnDisplay.text = $"Turn {turn}\n{factions[phase].name}'s Phase";
+        }
+
+        private void Select_performed(InputAction.CallbackContext obj) {
+            if (gridHovered && factions[phase].isPlayer) {
+                factions[phase].Characters[0].agent.SetDestination(hoveredPosition);
+                gridSelector.SetActive(false);
+                NextPhase();
+            }
+        }
+    }
+
+    public class Faction {
+        public readonly string name;
+        public readonly bool isPlayer;
+
+        List<GridPlayerController> characters;
+
+        public GridPlayerController[] Characters { get => characters.ToArray(); }
+
+        public Faction(GridPlayerController[] chars, bool player = false, string name = "Player") {
+            this.name = name;
+            characters = new List<GridPlayerController>(chars);
+            isPlayer = player;
+        }
+    }
+}
--- Assets/Scripts/GridPlayerController.cs
+++ Assets/Scripts/GridPlayerController.cs
@@ -1,23 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
 
 public class GridPlayerController : MonoBehaviour {
     public NavMeshAgent agent;
     public Animator animator;
 
-    private Controls controls;
-    private Vector2 mousePosition;
     private bool lastCondition = false;
 
-    private void Start() {
-        controls = new Controls();
-        controls.UI.Select.performed += Select_performed;
-        controls.UI.PointerPosition.performed += ctx => mousePosition = ctx.ReadValue<Vector2>();
-        controls.Enable();
-    }
-
     private void Update() {
+        // Set animation
         if (agent.hasPath != lastCondition) {
             lastCondition = agent.hasPath;
             if (lastCondition) {
@@ -25,18 +16,6 @@ public class GridPlayerController : MonoBehaviour {
             } else {
                 animator.SetTrigger("Stop Walk");
             }
-        }
-    }
-
-    private void Select_performed(InputAction.CallbackContext obj) {
-        var ray = Camera.main.ScreenPointToRay(mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit)) {
-            var position = hit.point;
-            position.x = Mathf.Round(position.x);
-            position.z = Mathf.Round(position.z);
-            agent.SetDestination(position);
         }
     }
 }

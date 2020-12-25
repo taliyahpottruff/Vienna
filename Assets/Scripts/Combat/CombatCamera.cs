58e18e4ae6d502bbb4501c3fe0a4af6192e25f11
Trenton Pottruff <tpottruff@gmail.com>
2020-12-25 01:43:07 -0500

    Moveable camera

--- Assets/Scripts/Combat/CombatCamera.cs
+++ Assets/Scripts/Combat/CombatCamera.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Combat {
-    [RequireComponent(typeof(Camera))]
     public class CombatCamera : MonoBehaviour {
-        public Camera camera;
-        public Vector3 target;
+        [Range(0f, 10f)]
+        public float speed = 4f;
+
+        private Controls controls;
+        private Vector2 inputVector;
+
+        private void Start() {
+            controls = new Controls();
+            controls.Player.Move.performed += ctx => inputVector = ctx.ReadValue<Vector2>();
+            controls.Enable();
+        }
+
+        private void Update() {
+            if (inputVector != Vector2.zero) {
+                var forwardDirection = Vector3.Normalize(new Vector3(transform.forward.x, transform.position.y, transform.forward.z));
+                var forward2D = new Vector2(forwardDirection.x, forwardDirection.z);
+                var side2D = Vector2.Perpendicular(forward2D) * -inputVector.x;
+                transform.Translate(forwardDirection * inputVector.y * speed * Time.deltaTime, Space.World);
+                transform.Translate(new Vector3(side2D.x, 0, side2D.y) * speed * Time.deltaTime, Space.World);
+            }
+        }
     }
 }
